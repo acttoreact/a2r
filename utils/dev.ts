7c38a196552f84ef6cf301344ce55cb2f301b33d
miguelBinpar <miguel@binpar.com>
2020-07-16 19:39:52 +0200

    dev command working

--- utils/dev.ts
+++ utils/dev.ts
@@ -7,16 +7,26 @@ import exec from '../tools/exec';
 import { log, fullPath } from './colors';
 import { ProjectInfo, DockerInfo } from '../model';
 
-const runProjects = async (mainProjectPath: string, watcher: DockerInfo, projects: ProjectInfo[]): Promise<void> => {
+const runProjects = async (
+  mainProjectPath: string,
+  watcher: DockerInfo,
+  projects: ProjectInfo[],
+): Promise<void> => {
   const pendingProjects = projects.slice();
   const project = pendingProjects.shift();
   if (project) {
     const { path: projectPath, type } = project;
-    const command = `run -it -d -v ${mainProjectPath}/server:/usr/src/app/bin/server -v ${mainProjectPath}/${projectPath}/.a2r/proxy:/usr/src/app/bin/.a2r/proxy --name ${watcher.name} ${watcher.imageName}:${watcher.version}`;
+    const name = `${watcher.name}-${projectPath}`;
+    await exec('docker', ['stop', name]);
+    await exec('docker', ['rm', name]);
+    const command = `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -v ${mainProjectPath}/${projectPath}/.a2r/proxy:/usr/src/app/.a2r/proxy --name ${name} ${watcher.imageName}:${watcher.version}`;
     out.info(command);
-    await exec('docker', command.split(' '));
+    const commandRes = await exec('docker', command.split(' '));
+    log(`Exit code: ${commandRes.code}. ${commandRes.out}`);
     if (type === 'next') {
-      await exec('npm', ['run', 'dev'], { cwd: path.resolve(mainProjectPath, projectPath) });
+      await exec('npm', ['run', 'dev'], {
+        cwd: path.resolve(mainProjectPath, projectPath),
+      });
     }
   }
   if (pendingProjects.length) {
@@ -30,20 +40,17 @@ const dev = async (): Promise<void> => {
   log(`Running watchers and frameworks at ${fullPath(mainProjectPath)}`);
   log(`Stopping running dockers...`);
   const { watcher, devServer, projects } = settings;
-  try {
-    await exec('docker', ['stop', watcher.name]);
-    await exec('docker', ['rm', watcher.name]);
-    await exec('docker', ['stop', devServer.name]);
-    await exec('docker', ['rm', devServer.name]);
-  } catch (ex) {
-    out.verbose(
-      'Error while stopping dockers, probably there were no dockers running',
-    );
-  }
-  log(`Starting dockers...`);
-  await exec('docker', [
-    `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -p 4000:4000 --name ${devServer.name} ${devServer.imageName}:${devServer.version}`,
-  ]);
+  await exec('docker', ['stop', devServer.name]);
+  await exec('docker', ['rm', devServer.name]);
+  log(`Starting server...`);
+  const serverResponse = await exec(
+    'docker',
+    `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -p 4000:4000 --name ${devServer.name} ${devServer.imageName}:${devServer.version}`.split(
+      ' ',
+    ),
+  );
+  log(`Exit code: ${serverResponse.code}. ${serverResponse.out}`);
+  log(`Starting projects and watchers...`);
   await runProjects(mainProjectPath, watcher, projects);
 };
 
