6649fd1e39e844dd1437f0ffb9239887d489c305
miguelBinpar <miguel@binpar.com>
2020-07-17 16:23:11 +0200

    working

--- utils/dev.ts
+++ utils/dev.ts
@@ -1,39 +1,13 @@
-import path from 'path';
 import { out } from '@a2r/telemetry';
 
 import { getSettings } from './settings';
 import getProjectPath from './getProjectPath';
 import exec from '../tools/exec';
 import { log, fullPath } from './colors';
-import { ProjectInfo, DockerInfo } from '../model';
 
-const runProjects = async (
-  mainProjectPath: string,
-  watcher: DockerInfo,
-  projects: ProjectInfo[],
-): Promise<void> => {
-  const pendingProjects = projects.slice();
-  const project = pendingProjects.shift();
-  if (project) {
-    const { path: projectPath, type } = project;
-    const name = `${watcher.name}-${projectPath}`;
-    await exec('docker', ['stop', name]);
-    await exec('docker', ['rm', name]);
-    const command = `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -v ${mainProjectPath}/${projectPath}/.a2r/proxy:/usr/src/app/.a2r/proxy --name ${name} ${watcher.imageName}:${watcher.version}`;
-    out.info(command);
-    const commandRes = await exec('docker', command.split(' '));
-    log(`Exit code: ${commandRes.code}. ${commandRes.out}`);
-    if (type === 'next') {
-      await exec('npm', ['run', 'dev'], {
-        cwd: path.resolve(mainProjectPath, projectPath),
-      });
-    }
-  }
-  if (pendingProjects.length) {
-    await runProjects(mainProjectPath, watcher, pendingProjects);
-  }
-};
-
+/**
+ * Temporary `dev` command that will run all needed dockers for solution
+ */
 const dev = async (): Promise<void> => {
   const settings = await getSettings();
   const mainProjectPath = await getProjectPath();
@@ -43,15 +17,25 @@ const dev = async (): Promise<void> => {
   await exec('docker', ['stop', devServer.name]);
   await exec('docker', ['rm', devServer.name]);
   log(`Starting server...`);
-  const serverResponse = await exec(
+  await exec(
     'docker',
     `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -p 4000:4000 --name ${devServer.name} ${devServer.imageName}:${devServer.version}`.split(
       ' ',
     ),
   );
-  log(`Exit code: ${serverResponse.code}. ${serverResponse.out}`);
-  log(`Starting projects and watchers...`);
-  await runProjects(mainProjectPath, watcher, projects);
+  await Promise.all(
+    projects.map(async (project) => {
+      const { path: projectPath } = project;
+      log(`Starting project ${projectPath}...`);
+      const name = `${watcher.name}-${projectPath}`;
+      await exec('docker', ['stop', name]);
+      await exec('docker', ['rm', name]);
+      const command = `run -it -d -v ${mainProjectPath}/server:/usr/src/app/server -v ${mainProjectPath}/${projectPath}/.a2r/proxy:/usr/src/app/.a2r/proxy --name ${name} ${watcher.imageName}:${watcher.version}`;
+      out.info(command);
+      await exec('docker', command.split(' '));
+    }),
+  );
+  log(`All dockers and projects are running`);
 };
 
 export default dev;
